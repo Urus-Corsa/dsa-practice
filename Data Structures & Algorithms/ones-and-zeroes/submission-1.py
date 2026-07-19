1eba6e4569929c1b4d79038f8a3f5e60343ec9a5
Sina Kalantar <sinakalantar09@gmail.com>
2026-07-18 21:03:53 -0700

    Add: ones-and-zeroes - submission-1

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        char_count = defaultdict(tuple)
+        for i, str in enumerate(strs):
+            zeros = 0
+            ones = 0
+            for ch in str:
+                if ch == '0':
+                    zeros += 1
+                else:
+                    ones += 1
+            char_count[i] = (zeros, ones)
+        cache = {}
+        def recurse(curr_i, zeros_count, ones_count, strs_count):
+            if (curr_i, zeros_count, ones_count, strs_count) in cache:
+                return cache[(curr_i, zeros_count, ones_count, strs_count)]
+            if curr_i == len(strs) and zeros_count <= m and ones_count <= n:
+                return strs_count
+            if curr_i >= len(strs) or zeros_count > m or ones_count > n:
+                return 0
+            include = 0
+            i_zeros, i_ones = char_count[curr_i]
+            if i_zeros+zeros_count <= m and i_ones+ones_count <= n:
+                include = recurse(curr_i+1, zeros_count+i_zeros, i_ones+ones_count, strs_count+1)
+            exclude = recurse(curr_i+1, zeros_count, ones_count, strs_count)
+            cache[(curr_i, zeros_count, ones_count, strs_count)] = max(include, exclude)
+            return cache[(curr_i, zeros_count, ones_count, strs_count)]
+        return recurse(0,0,0,0)
